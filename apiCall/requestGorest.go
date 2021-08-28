6d27760f6d1d430c15baf937801f0024065c1f95
Nupur Thakur <nupur@gather.network>
2021-08-28 13:06:00 +0530

    Concurrency using waitgroup

--- apiCall/requestGorest.go
+++ apiCall/requestGorest.go
@@ -3,11 +3,11 @@ package apiCall
 import (
 	"Test/model"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
+	"sync"
 )
 
 type API struct {
@@ -15,25 +15,27 @@ type API struct {
 	BaseURL string
 }
 
-func (a *API) GetDataFromGoRest(pageNumber int) []model.User {
+func (a *API) GetDataFromGoRest(pageNumber int, wg *sync.WaitGroup) []model.User {
 	res, err := http.Get(a.BaseURL + "/public/v1/users?page=" + strconv.Itoa(pageNumber))
+	defer wg.Done()
 	if err != nil {
-		fmt.Println("unable to make the request", err)
-	}
-	// fmt.Println(res)
-	if res.StatusCode != 200 {
-		log.Println("unable to get the data", res.StatusCode)
-		return nil
+		log.Println("unable to make the request", err)
 	}
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return nil
 	}
+	if res.StatusCode != 200 {
+		log.Println(string(body))
+		log.Println("unable to get the data", res.StatusCode)
+		return nil
+	}
+
 	var data model.Data
 	err = json.Unmarshal(body, &data)
 	if err != nil {
-		fmt.Println("uanble to parse json", err)
+		log.Println("uanble to parse json", err)
 		return nil
 	}
 
